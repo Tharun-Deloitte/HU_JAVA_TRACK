d8375a8023c667ea0600fd6dfaeb915a902d8a15
Reddy <naratreddy@deloitte.com>
2022-03-17 12:17:17 +0530

    Mini Assignment 3

--- Arrays_And_Strings_Assignment/src/srings.java
+++ Arrays_And_Strings_Assignment/src/srings.java
@@ -10,7 +10,6 @@ public class srings {
         if (a + b != str3.length()) {
             System.out.println("Invalid");
         } else {
-            int f = 0;
             int i = 0, j = 0, k = 0;
             while (k < str3.length()) {
                 if (i < str1.length() && str1.charAt(i) == str3.charAt(k))
@@ -18,7 +17,6 @@ public class srings {
                 else if (j < str2.length() && str2.charAt(j) == str3.charAt(k))
                     j++;
                 else {
-                    f = 1;
                     break;
                 }
                 k++;
